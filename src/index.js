b7f2ef42801ae6b136fa65a8db762074a43294f0
taiwocoker <taiwofcoker@gmail.com>
2020-09-14 23:33:44 +0100

    Configure phaser for production

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(80)
+import 'phaser';
+
+import { SimpleScene } from './simple-scene';
+
+const gameConfig = {
+  width: 680,
+  height: 400,
+  scene: SimpleScene
+};
+
+new Phaser.Game(gameConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {app:'./src/index.js',
+  'production-dependencies': ['phaser']
+},
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true)
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'production-dependencies',
+      filename: 'production-dependencies.bundle.js'
+    }),
+
+  ]
 };
