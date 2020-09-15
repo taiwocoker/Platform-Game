70fd7dc831c0a95ee051ab8515bb55e889d6e3ce
taiwocoker <taiwofcoker@gmail.com>
2020-09-15 16:04:02 +0100

    Add different scenes for the game

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,13 @@ export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 1500,
-  height: 800
+  height: 800,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
 };
